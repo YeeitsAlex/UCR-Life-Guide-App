0510bd8e38ea164115c1fbb5017dc0d5698be211
Alexander Yee <ayee013@ucr.edu>
2020-05-29 13:12:06 -0700

    Add firebase API keys to config.js

---
+++ UCRLifeGuide/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAjiPNybkiq3YnK9LyySsg9Bbsf0rHDPPE",
+    authDomain: "ucrlifeguide.firebaseapp.com",
+    databaseURL: "https://ucrlifeguide.firebaseio.com",
+    projectId: "ucrlifeguide",
+    storageBucket: "ucrlifeguide.appspot.com",
+    messagingSenderId: "930611404938",
+    appId: "1:930611404938:web:3caa9fb2fb57b2c389cd73"
+  };
+
+  const FirebaseKeys = firebaseConfig;
+  export default FirebaseKeys;
