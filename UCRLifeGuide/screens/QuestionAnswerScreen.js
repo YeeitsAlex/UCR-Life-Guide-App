9c25c235790a0c9454818eab086ebce5c0656623
Alexander Yee <ayee013@ucr.edu>
2020-06-02 17:26:40 -0700

    Update QuestionAnswer to show static feed

--- UCRLifeGuide/screens/QuestionAnswerScreen.js
+++ UCRLifeGuide/screens/QuestionAnswerScreen.js
@@ -1,25 +1,78 @@
 import React from 'react'
-import {View, Text, StyleSheet, TouchableOpacity, ImageBackground, StatusBar, LayoutAnimation, Image} from 'react-native'
-import * as firebase from 'firebase'
+import {View, Text, StyleSheet, TouchableOpacity, ImageBackground, StatusBar, LayoutAnimation, Image, FlatList} from 'react-native'
+import firebase from 'firebase'
+import {Ionicons} from '@expo/vector-icons'
+import moment from 'moment'
+import Fire from "../Fire"
+// import { FlatList } from 'react-native-gesture-handler';
+
+// const firebase = require('firebase');
+require("firebase/firestore");
+
+//temp posts
+const posts = [
+    {
+        id: "1",
+        name: "John Doe",
+        text: "Hello there",
+        timestamp: "Tue Jun 02 2020 14:27:32 GMT-0700 (PDT)",
+    },
+    {
+        id: "2",
+        name: "Jane Doe",
+        text: "It's hot",
+        timestamp: "1590794172334"
+    },
+];
 
 export default class QuestionAnswerScreen extends React.Component{
-    //Some changes to navigationOptions won't be recognized until we reload the app
-    // static navigationOptions ={
-    //     headerShown: false
-    // };
+    renderPost = post => {
+        return(
+            <View style={styles.postItem}>
+                <Image source = {require("../assets/question.png")} style={styles.question}></Image>
+                <View style={{flex: 1}}>
+                    <View style={{flexDirection: "row", justifyContent: "space-between", alignItems:"center"}}>
+                        <View>
+                            <Text style={styles.names}>{post.name}</Text>
+                            {/* <Text style={styles.timestamp}>{moment(post.timestamp).toDate()}</Text> */}
+                            <Text style={styles.timestamp}>{(post.timestamp)}</Text>
+                            <Text style={styles.postText}>{post.text}</Text>
+                        </View>
 
-    // signOutUSer = () => {
-    //     firebase.auth().signOut();
-    // };
+                    </View>
+                    <TouchableOpacity style={{flexDirection: "row", justifyContent: "flex-end", right: 15}}>
+                        <Ionicons name="ios-chatboxes" size={26} color="#73788B"></Ionicons>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    };
 
     render(){
-        // LayoutAnimation.easeInEaseOut();
         return(
             <View style={styles.container}>
-                <Text>Q&A Screen</Text>                
-                {/* <TouchableOpacity style={styles.circleButton} onPress= {() => this.props.navigation.navigate("Post")}>
-                    <Text style={styles.plus}>+</Text>
-                </TouchableOpacity> */}
+                <View style={styles.header}>
+                    <Text style={styles.title}>Q&A Forum</Text>                
+                </View>
+                
+                <Text style={styles.listHeader}>Frequently Asked Questions</Text>
+                <FlatList 
+                    style={styles.postsList} 
+                    data={posts} 
+                    renderItem={({item})=> this.renderPost(item)}
+                    keyExtractor={item => item.id}
+                    showsVerticalScrollIndicator={false}    
+                />
+
+                <Text style={styles.listHeader}>Recent Questions</Text>
+                <FlatList 
+                    style={styles.postsList} 
+                    data={posts} 
+                    renderItem={({item})=> this.renderPost(item)}
+                    keyExtractor={item => item.id}
+                    showsVerticalScrollIndicator={false}    
+                />
+
             </View>
         );
     }
@@ -28,96 +81,63 @@ export default class QuestionAnswerScreen extends React.Component{
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
+        backgroundColor: "#EFECF4"
     },
-    greeting:{
-        marginTop: 150,
-        fontSize: 35,
-        fontWeight: "bold",
-        textAlign: "center",
-        color: "#f8f8ff",
-    },
-    plus: {
-        color: "#FFF", 
-        fontWeight: "400",
-        fontSize: 28,
-        bottom: 7,
-        left: 4
-    },
-    errorMessage:{
-        height: 72,
+    header: {
+        paddingTop: 64,
+        paddingBottom: 16,
+        backgroundColor: "#FFF",
         alignItems: "center",
         justifyContent: "center",
-        marginHorizontal: 30,
+        borderBottomWidth: 1,
+        borderBottomColor: "#EBECF4",
+        shadowColor: "#454D65",
+        shadowOffset: { height: 5},
+        shadowRadius: 15,
+        shadowOpacity: 0.2,
+        zIndex: 10
     },
-    form:{
-        marginBottom: 48,
-        marginHorizontal: 30,
+    title: {
+        fontSize: 20,
+        fontWeight: "500"
     },
-    inputTitle:{
-        // marginTop: 50,
-        marginLeft: 15,
-        color: "#8A8F9E",
-        fontSize: 10,
-        textTransform: "uppercase"
+    postsList: {
+        marginHorizontal: 16
     },
-    input:{
-        borderBottomColor: "#8A8F9E",
-        borderBottomWidth: StyleSheet.hairlineWidth,
-        height: 40,
+    postItem: {
+        backgroundColor: '#FFF',
+        borderRadius: 5,
+        padding: 0, 
+        flexDirection: "row",
+        marginVertical: 10,
+        // height: 50
+    },
+    names: {
         fontSize: 15,
-        color: "#161F3D",
-        marginLeft: 15,
-        marginRight: 15,
-        marginBottom: 15,
-    },
-    inputPass:{
-        borderBottomColor: "#8A8F9E",
-        borderBottomWidth: StyleSheet.hairlineWidth,
-        height: 40,
-        fontSize: 15,
-        color: "#161F3D",
-        marginLeft: 15,
-        marginRight: 15,
-        marginTop: 32,
-    },
-    button:{
-        marginTop: 30,
-        marginHorizontal: 30,
+        fontWeight: "500", 
+        color: '#454D65'
+    }, 
+    listHeader:{
         backgroundColor: '#4169e1',
-        borderRadius: 4,
-        height: 52,
-        alignItems: "center",
-        justifyContent: "center"
+        fontSize: 20,
+        paddingLeft: 15,
+        color: '#FFF',
+        paddingTop: 10, 
     },
-    imageBackground:{
-        flex: 0.5,
-        width: null,
-        height: null,
-        resizeMode: 'contain',
-        opacity: .9,
-        marginBottom: -35,
-        marginTop: -35
+    timestamp:{
+        fontSize: 11,
+        color: "#C4C6CE",
+        marginTop: 4
     },
-    entryMessage: {
-        marginTop: 75,
-        // marginHorizontal: 150, commented out because textAlign accomplishes the same thing
-        textAlign: "center",
-        fontSize: 35,
-        
+    postText:{
+        marginTop: 16,
+        fontSize: 14,
+        color: '#838899'
     },
-    circleButton:{
-        alignSelf: 'flex-end',
-        position: "absolute",
-        right: 15,
-        bottom: 35,
-        padding: 10,
-        height: 45,
-        width: 45,  //The Width must be the same as the height
-        borderRadius:90, //Then Make the Border Radius twice the size of width or Height   
-        backgroundColor:'gold',
-    
-      }
-
+    question:{
+        width: 42,
+        height: 42,
+        borderRadius: 18,
+        marginRight: 16
+    }
 })
