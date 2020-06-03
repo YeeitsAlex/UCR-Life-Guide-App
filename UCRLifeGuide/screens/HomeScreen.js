a1b02db8e139cc185f1e643c00cafc905a80b27a
Alexander Yee <ayee013@ucr.edu>
2020-06-02 17:24:50 -0700

    Fix logout to work properly, add guest greeting for anonymous user

--- UCRLifeGuide/screens/HomeScreen.js
+++ UCRLifeGuide/screens/HomeScreen.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import {View, Text, StyleSheet, TouchableOpacity, ImageBackground, StatusBar, LayoutAnimation, Image} from 'react-native'
 import * as firebase from 'firebase'
+import Fire from '../Fire'
+
+require('firebase/firestore')
 
 export default class HomeScreen extends React.Component{
     //Some changes to navigationOptions won't be recognized until we reload the app
@@ -14,27 +17,41 @@ export default class HomeScreen extends React.Component{
     };
     
     componentDidMount(){
-        const {email, displayName} = firebase.auth().currentUser;
+        if(!firebase.auth().currentUser.isAnonymous){
+            const {email, displayName} = firebase.auth().currentUser;
 
-        this.setState({email, displayName});
+            this.setState({email, displayName});
+        }
     }
 
-    signOutUSer = () => {
-        firebase.auth().signOut();
+    signOutUser = () => {
+        console.log("Trying to logout..");
+        firebase.auth().signOut().then(() => console.log("User signed out!"));
     };
 
+    loggedInMessage = () => {
+        if(!firebase.auth().currentUser.isAnonymous){
+            return (<Text style={styles.entryMessage}>{'Welcome back, \n'}{this.state.displayName}!</Text>)
+        }
+        else{
+            return (<Text style={styles.guestEntryMessage}>{'Welcome!\n You are signed in as a Guest User'}!</Text>)
+        }
+    }
+
     render(){
-        // LayoutAnimation.easeInEaseOut();
+        LayoutAnimation.easeInEaseOut();
         return(
             <View style={styles.container}>
                 <StatusBar barStyle="light-content"></StatusBar>
                 <ImageBackground style= {styles.imageBackground} source={require("../assets/LifeAtUCR.png")}></ImageBackground>
 
-                <Text style={styles.entryMessage}>{'Welcome back, \n'}{this.state.displayName}!</Text>
+                {this.loggedInMessage()}
+                {/* <Text style={styles.entryMessage}>{'Welcome back, \n'}{this.state.displayName}!</Text> */}
 
-                <TouchableOpacity style={styles.button} onPress={this.signOutUser} onPress= {() => this.props.navigation.navigate("Login")}>
-                    <Text style={{color: "#FFF", fontWeight: "500"}}>Logout</Text>
+                <TouchableOpacity style={styles.button} onPress={() => this.signOutUser()}>
+                    <Text style={{color: "#FFF", fontWeight: "500", paddingBottom: 15, fontSize: 15}}>Logout</Text>
                 </TouchableOpacity>
+
                 
             </View>
         )
@@ -92,13 +109,18 @@ const styles = StyleSheet.create({
         marginTop: 32,
     },
     button:{
-        marginTop: 30,
-        marginHorizontal: 30,
+        flex: 1,
+        // marginTop: 30,
+        marginHorizontal: 83,
         backgroundColor: '#4169e1',
         borderRadius: 4,
         height: 52,
+        width: 250,
         alignItems: "center",
-        justifyContent: "center"
+        justifyContent: "flex-end",
+        position: 'absolute',
+        bottom: 0,
+        marginBottom: 32,
     },
     imageBackground:{
         flex: 0.5,
@@ -115,6 +137,21 @@ const styles = StyleSheet.create({
         textAlign: "center",
         fontSize: 35,
         
+    },
+    guestEntryMessage:{
+        marginTop: 75,
+        // marginHorizontal: 150, commented out because textAlign accomplishes the same thing
+        textAlign: "center",
+        fontSize: 35,
+    },
+    button2:{
+        marginTop: 30,
+        marginHorizontal: 30,
+        backgroundColor: '#4169e1',
+        borderRadius: 4,
+        height: 52,
+        alignItems: "center",
+        justifyContent: "center"
     },
 
 })
