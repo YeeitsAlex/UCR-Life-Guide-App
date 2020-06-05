0f17d9d2c0f07d4cb4afb8634638a4138f14fa5c
Alexander Yee <ayee013@ucr.edu>
2020-06-05 12:16:28 -0700

    Add some more housing options and enable scroll functionality

--- UCRLifeGuide/screens/HousingScreen.js
+++ UCRLifeGuide/screens/HousingScreen.js
@@ -1,37 +1,51 @@
 import React from 'react'
-import {View, Text, StyleSheet, TouchableOpacity, ImageBackground, StatusBar, LayoutAnimation, Image, SafeAreaView} from 'react-native'
+import {View, Text, StyleSheet, TouchableOpacity, ImageBackground, StatusBar, LayoutAnimation, Image, SafeAreaView, ScrollView, Dimensions} from 'react-native'
 import * as firebase from 'firebase'
 
+const {height} = Dimensions.get('window')  
 
 export default class HousingScreen extends React.Component{
+    state={
+        screenHeight: 0,
+    }
     //Some changes to navigationOptions won't be recognized until we reload the app
     static navigationOptions ={
          headerShown: false
     };
 
-    // signOutUSer = () => {
-    //     firebase.auth().signOut();
-    // };
+    onContentSizeChange = (contentWidth, contentHeight) =>{
+        this.setState({screenHeight: contentHeight});
+    }
 
     render(){
+        const scrollEnabled = this.state.screenHeight > height;
         // LayoutAnimation.easeInEaseOut();
         return(
-            <SafeAreaView >
+            <SafeAreaView>
+                <ScrollView onContentSizeChange={this.onContentSizeChange}>
                 <View style={styles.headers}>
                     <Text style={styles.title}>Housing Options</Text>                
                 </View>
                 
+                {/* On Campus Housing */}
                 <Text style={styles.header}>On Campus Housing</Text>
                 <TouchableOpacity style={styles.button1} onPress={() => this.props.navigation.navigate("GlenMor")}>
                     <Text style={styles.housingNames}>Glen Mor</Text>
                 </TouchableOpacity>               
                 <TouchableOpacity style={styles.button1} onPress={() => this.props.navigation.navigate("Bannockburn")}>
-                    <Text style={styles.housingNames}>Bannockburn</Text>
+                    <Text style={styles.housingNames}>Bannockburn Village</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.button1} onPress={() => this.props.navigation.navigate("Stonehaven")}>
                     <Text style={styles.housingNames}>Stonehaven</Text>
                 </TouchableOpacity>
+                <TouchableOpacity style={styles.button1} onPress={() => this.props.navigation.navigate("Stonehaven")}>
+                    <Text style={styles.housingNames}>Falkirk</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button1} onPress={() => this.props.navigation.navigate("Stonehaven")}>
+                    <Text style={styles.housingNames}>The Plaza</Text>
+                </TouchableOpacity>
 
+                {/* Off Campus Housing */}
                 <Text style={styles.header}>Off Campus Housing</Text>
                 <TouchableOpacity style={styles.button1} onPress={() => this.props.navigation.navigate("GrandMarc")}>
                     <Text style={styles.housingNames}>Grand Marc</Text>
@@ -42,6 +56,13 @@ export default class HousingScreen extends React.Component{
                 <TouchableOpacity style={styles.button1} onPress={() => this.props.navigation.navigate("CampusCrossing")}>
                     <Text style={styles.housingNames}>Campus Crossing</Text>
                 </TouchableOpacity>
+                <TouchableOpacity style={styles.button1} onPress={() => this.props.navigation.navigate("CampusCrossing")}>
+                    <Text style={styles.housingNames}>Berkdale</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button1} onPress={() => this.props.navigation.navigate("CampusCrossing")}>
+                    <Text style={styles.housingNames}>University Towers</Text>
+                </TouchableOpacity>
+                </ScrollView>
             </SafeAreaView>
         );
     }
